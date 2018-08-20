d108a1030ace7482dbba740b04d86e989923f65f
Dozer <birsv@list.ru>
2018-08-20 21:34:57 +0300

    add

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const ADDRES = 'ADDRES';
 
 const atADDRES= new Scene(ADDRES);
 
- 
-
 const { button, reply } = Alice
-
- 
-
- 
 
 /*alice.command(async (ctx) => {
 
@@ -36,77 +30,41 @@ const { button, reply } = Alice
 
 });*/
 
- 
-
 atADDRES.command('Выйти', async ctx => {
-
   ctx.leave();
-
   return Reply.text('Добрый день! Меня зовут Алиса, я могу проконсультировать Вас по продуктам/услугам Банка! Чтобы выбрать интересующий Вас раздел, повторите, пожалуйста, его название за мной:',
-
   { // Extra params are optional
-
     tts: 'Добрый день! Меня зовут Алиса- я могу проконсультировать Вас по продуктам/услугам Банка! Чтобы выбрать интересующий Вас раздел, повторите, пожалуйста, его название за мной:',
-
     buttons: ['one', Markup.button({
-
     title: 'two',
-
     url: '',
-
     payload: {},
-
     hide: false
-
   })],
-
     end_session: true
-
   });
-
 });
-
  
-
 atADDRES.any(ctx => Reply.text(`Не понимаю что Вы сказали. Чтобы выйти скажите "Выйти"`),
-
 {
-
   tts: `Не понимаю что Вы сказали. Чтобы выйти скажите Выйти`
-
 });
-
- 
 
 stage.addScene(atADDRES);
 
 alice.use(stage.getMiddleware());
 
- 
-
 alice.command('Адреса режимы работы ОО', ctx => {
-
   ctx.enter(ADDRES);
-
   return Reply.text('Ниже приведены возможные вопросы: ', { tts:'Ниже приведены возможные вопросы: ',
-
     buttons: ['Назовите адрес отделения/банкомата/терминалов самообслуживания/ PoS-терминалов?', 'Где/ в каких магазинах я могу расплатиться Вашей картой?',
-
               'Куда я могу подойти для …', 'Назовите Адреса РНКБ', 'Где находится Головной/Главный офис РНКБ?', 'Режим работы','Режим работы Контактного центра /Колл-центра', 'Выйти'],
-
   });
-
 });
-
- 
 
 alice.any(async ctx => Reply.text(`I don't understand`));
 
- 
-
 const server = alice.listen(8090, '/');
-
- 
 
 // Put a friendly message on the terminal
 
