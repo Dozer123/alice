7ca1d412a854fe37cb495bbfbbec4dc8df963835
Sergey <birsv@list.ru>
2018-08-26 21:08:49 +0300

    Add

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ fs.readFile('./control.html', function (err, html) {
       response.writeHeader(200, {"Content-Type": "text/html"});  
       response.write(html);  
       response.end();  
-  }).listen(8000);
+  }).listen(80);
 });
 
 console.log('Server running at http://127.0.0.1:' + 8090 + '/');
